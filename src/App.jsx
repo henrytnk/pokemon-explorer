10fa1137ead038f3ae869bc2a61f3972f1b12ccb
henrytnk <henrytnk@gmail.com>
2025-02-12 20:46:23 -0300

    Add Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import pokeballLogo from './assets/pokeball-bg.webp'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src={pokeballLogo} className="logo react" alt="React logo" />
+    </>
+  )
+}
+
+export default App
